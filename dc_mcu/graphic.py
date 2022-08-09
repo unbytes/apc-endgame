e1938a93f37717646ff8413f566f67161e0a4ad7
marcomarquesdc <marcomarquesdecastro@gmail.com>
2022-08-09 08:35:33 -0300

    code: reading dc/mcu data

--- dc_mcu/graphic.py
+++ dc_mcu/graphic.py
@@ -1,5 +1,10 @@
 import pandas as pd
 import plotly.express as px
 
+# Caminho para os dados
 PATH_DC_DATASET = "assets\data\dc-wikia-data.csv"
 PATH_MARVEL_DATASET = "assets\data\marvel-wikia-data.csv"
+
+# Leitura dos dados CSV 
+dc_dataframe = pd.read_csv(PATH_DC_DATASET)
+mcu_dataframe = pd.read_csv(PATH_MARVEL_DATASET)
