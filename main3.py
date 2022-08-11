252ca81442236619fd80e555ef2004346c265750
Bianca Patrocínio <70040539+BiancaPatrocinio7@users.noreply.github.com>
2022-08-11 09:59:24 -0300

    código comentado

--- main3.py
+++ main3.py
@@ -1,17 +1,22 @@
 import pandas as pd
+import plotly.graph_objects as go
+#importar o arquivo
 dados = pd.read_csv("mcu_box_office.csv")
+#variável de 100% do gráfico
 value_duration = 0
+#somando todos os minutos
 for data in dados.movie_duration:
     value_duration += data
+#variável de cada porcentagem por filme 
 movie_porcent = [ ]
+#cálculo de porcentagem 
 for duration in dados.movie_duration:
     porcent = (100*duration)/value_duration
     movie_porcent.append(porcent)
+#guardar os nomes dos filmes em um vetor
 movie_names = [ ]
 for name in dados.movie_title:
     movie_names.append(name)
-    
-print(movie_names)
-print(movie_porcent)    
-print(value_duration)
-dados["movie_duration"].value_counts()
+
+#gráfico
+fig = go.Figure(data=[go.Pie(labels=movie_names, values=movie_porcent)])
