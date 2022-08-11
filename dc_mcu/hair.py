e93e3f1d4d8ab3608b2453d7ff01041f73657b9d
Mateus Vieira <68292695+MateusVrs@users.noreply.github.com>
2022-08-10 21:33:34 -0300

    code: improve hair data algorithm manipulation

--- dc_mcu/hair.py
+++ dc_mcu/hair.py
@@ -1,23 +1,29 @@
 from graphic import *
 
-# Contagem das cores dos cabelos (DC)
-dc_hair_color_count = dict() # Armazenamento dos dados de cor de cabelo
-for hair_color in dc_dataframe.HAIR : # Iterar sobre a coluna hair (cor de cabelo)
-    if hair_color in dc_hair_color_count.keys(): # Inicializar chave caso não exista
-        dc_hair_color_count[hair_color] += 1 # Soma +1 na cor de cabelo
-    else:
-        dc_hair_color_count[hair_color] = 1 # Cria chave
+MINIMUM_CHARACTERS = 50
 
-# Contagem das cores dos cabelos (Marvel)
-mcu_hair_color_count = dict() # Armazenamento dos dados de cor de cabelo
-for hair_color in mcu_dataframe.HAIR : # Iterar sobre a coluna hair (cor de cabelo)
-    if hair_color in mcu_hair_color_count.keys(): # Inicializar chave caso não exista
-        mcu_hair_color_count[hair_color] += 1 # Soma +1 na cor de cabelo
-    else:
-        mcu_hair_color_count[hair_color] = 1 # Cria chave
+# Contagem das cores dos cabelos (DC)
+dc_hair_colors = set(dc_dataframe.HAIR)
+dc_hair_color_count = dict()  # Armazenamento da contagem
+
+for hair_color in dc_hair_colors:  # Iterar sobre as cores de cabelo e filtrar
+    color_count = list(dc_dataframe.HAIR).count(hair_color)
+    if color_count >= MINIMUM_CHARACTERS and type(hair_color) == str:
+        dc_hair_color_count[hair_color] = color_count
+
+# Marvel
+mcu_hair_colors = set(mcu_dataframe.HAIR)
+mcu_hair_color_count = dict()
+
+for hair_color in mcu_hair_colors:
+    color_count = list(mcu_dataframe.HAIR).count(hair_color)
+    if color_count >= MINIMUM_CHARACTERS and type(hair_color) == str:
+        mcu_hair_color_count[hair_color] = color_count
 
 # Gerenciamento de dados
-dc_hair_color_number, mcu_hair_color_number = len(dc_hair_color_count.keys()), len(mcu_hair_color_count.keys())
+dc_hair_color_number = len(dc_hair_color_count.keys())
+mcu_hair_color_number = len(mcu_hair_color_count.keys())
+
 dc_mcu_hair_colors = list(dc_hair_color_count.keys()) + list(mcu_hair_color_count.keys())
 dc_mcu_hair_color_count = list(dc_hair_color_count.values()) + list(mcu_hair_color_count.values())
 
@@ -25,9 +31,9 @@ dc_mcu_hair_color_count = list(dc_hair_color_count.values()) + list(mcu_hair_col
 hair_color_count = {
     'colors': dc_mcu_hair_colors,
     'count': dc_mcu_hair_color_count,
-    'world': ['dc']*dc_hair_color_number + ['mcu']*mcu_hair_color_number
+    'world': ['DC']*dc_hair_color_number + ['MCU']*mcu_hair_color_number
 }
 
 # Plotagem do gráfico
-fig = px.bar(hair_color_count, x='world', y='count', color='colors', barmode='group')
+fig = px.bar(hair_color_count, x='world', y='count', color='colors', color_discrete_map='identity', barmode='group')
 fig.show()
