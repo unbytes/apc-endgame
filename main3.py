bf667077c569e8ee2c8b5a4e320647233b616735
BiancaPatrocinio7 <biancapatrocinio774@gmail.com>
2022-08-10 21:40:14 -0300

    calculos pizza

--- main3.py
+++ main3.py
@@ -1,4 +1,17 @@
-import panda as pd 
-import plotly.express as px
-
-movie_duration = pd.read_csv("C:\Unb\APC\apc-endgame\MCU_movies_Dataset.csv")
+import pandas as pd
+dados = pd.read_csv("mcu_box_office.csv")
+value_duration = 0
+for data in dados.movie_duration:
+    value_duration += data
+movie_porcent = [ ]
+for duration in dados.movie_duration:
+    porcent = (100*duration)/value_duration
+    movie_porcent.append(porcent)
+movie_names = [ ]
+for name in dados.movie_title:
+    movie_names.append(name)
+    
+print(movie_names)
+print(movie_porcent)    
+print(value_duration)
+dados["movie_duration"].value_counts()
