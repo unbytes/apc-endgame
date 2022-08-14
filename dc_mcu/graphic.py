2e33fe41f8635678a2ed7c57ef6dd14fca984b57
Mateus Vieira <68292695+MateusVrs@users.noreply.github.com>
2022-08-14 17:56:11 -0300

    code: improve dc/mcu graphic algorithm

--- dc_mcu/graphic.py
+++ dc_mcu/graphic.py
@@ -13,45 +13,56 @@ mcu_dataframe = pd.read_csv(PATH_MARVEL_DATASET)
 GRAPHIC_HAIR_COLUMN = 'HAIR'
 GRAPHIC_EYE_COLUMN = 'EYE'
 
-# Faz todas as operações sobre uma respectiva coluna de características para plotar o gráfico
-def generate_graphic_column_based(df_column, title):
-  # Retorna uma lista de cores de uma coluna
-  def extract_colors(dataframe, column):
-      colors = list() # Lista de cores
-      for color in dataframe[column]:
-          if color not in colors and type(color) == str: # Verifica se a cor já está na lista e se é válida (str)
-              colors.append(color) # Adiciona cor
-      return colors
+def generate_graphic_column_based(bar_group_order, dataframes, column_title, graphic_title):
+    """
+    Gera um gráfico comparativo entre dois ou mais DataFrames com base em uma coluna
+    """
+    def extract_colors(dataframe):
+        """
+        Recebe um DataFrame e extrai as cores da coluna indicada na função pai
+        """
+        colors = list() # Lista de cores
+        for color in dataframe[column_title]:
+            if color not in colors and type(color) == str: # Verifica se a cor já está na lista e se é válida (str)
+                colors.append(color) # Adiciona cor
+        return colors 
 
-  dc_colors = extract_colors(dc_dataframe, df_column)
-  mcu_colors = extract_colors(mcu_dataframe, df_column)
+    colors_matrix = list()
+    for dataframe in dataframes:
+        colors_matrix.append(extract_colors(dataframe))
 
-  # Verifica as cores em comum e constroi uma lista
-  colors_in_common = list()
-  for color in dc_colors:
-      if color in mcu_colors and color not in colors_in_common:
-          colors_in_common.append(color)
-  for color in mcu_colors:
-      if color in dc_colors and color not in colors_in_common:
-          colors_in_common.append(color)
+    # Verifica as cores em comum e constroi um conjunto
+    if len(colors_matrix) <= 0:
+        return None
+    else:
+        colors_in_common = set(colors_matrix[0])
+        for colors in colors_matrix[1:]:
+            colors_in_common = colors_in_common.union(set(colors))
+    colors_in_common = list(colors_in_common)
 
-  # Retorna a lista de contagem das cores
-  def count_color_appearance(dataframe, colors, column):
-      count = list() # Lista de contagem
-      for color in colors:
-          count.append(list(dataframe[column]).count(color)) # Adiciona contagem de uma cor
-      return count
+    def count_color_appearance(dataframe, colors):
+        """
+        Retorna a lista do número de vezes que cada cor aparece na coluna do DataFrame
+        """
+        count = list() # Lista de contagem
+        for color in colors:
+            count.append(list(dataframe[column_title]).count(color)) # Adiciona contagem de uma cor
+        return count
 
-  dc_count = count_color_appearance(dc_dataframe, colors_in_common, df_column)
-  mcu_count = count_color_appearance(mcu_dataframe, colors_in_common, df_column)
- 
-  # Plotagem do gráfico
-  graphic_figure = go.Figure([
-      go.Bar(name='DC', x=colors_in_common, y=dc_count),
-      go.Bar(name='MCU', x=colors_in_common, y=mcu_count)
-  ])
-  graphic_figure.update_layout(title_text=title, yaxis_range=[0, max(max(dc_count), max(mcu_count))])
-  graphic_figure.show()
+    count_matrix = list()
+    for dataframe in dataframes:
+        count_matrix.append(count_color_appearance(dataframe, colors_in_common))
+    
+    # Plotagem do gráfico
+    graphic_bars = list()
+    for count, bar_group in zip(count_matrix, bar_group_order):
+        graphic_bars.append(go.Bar(name=bar_group, x=colors_in_common, y=count))
+        
+    graphic_figure = go.Figure(graphic_bars)
+    graphic_figure.update_layout(title_text=graphic_title)
+    graphic_figure.show()
 
-generate_graphic_column_based(GRAPHIC_HAIR_COLUMN, 'Cor de cabelo: Personagens da Marvel e DC')
-generate_graphic_column_based(GRAPHIC_EYE_COLUMN, 'Cor dos olhos: Personagens Marvel e DC')
+dataframes = [dc_dataframe, mcu_dataframe]
+group_order = ['DC', 'MCU']
+generate_graphic_column_based(group_order, dataframes, GRAPHIC_HAIR_COLUMN, 'Cor de cabelo: Personagens da Marvel e DC')
+generate_graphic_column_based(group_order, dataframes, GRAPHIC_EYE_COLUMN, 'Cor dos olhos: Personagens Marvel e DC')
