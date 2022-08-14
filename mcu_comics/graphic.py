6b616ea82bcfe35279889d39ac64fe99056fb275
Mateus Vieira <68292695+MateusVrs@users.noreply.github.com>
2022-08-14 18:01:37 -0300

    code: rating title const

--- mcu_comics/graphic.py
+++ mcu_comics/graphic.py
@@ -5,14 +5,16 @@ PATH_MARVEL_COMICS = 'assets\data\Marvel_Comics.csv'
 
 comics_dataframe = pd.read_csv(PATH_MARVEL_COMICS)
 
+RATING_COLUMN_TITLE = 'Rating'
+
 comics_rating = list()
-for rating in comics_dataframe['Rating']:
+for rating in comics_dataframe[RATING_COLUMN_TITLE]:
     if rating not in comics_rating:
         comics_rating.append(rating)
 
 rating_count = list()
 for rating in comics_rating:
-    rating_count.append(list(comics_dataframe['Rating']).count(rating))
+    rating_count.append(list(comics_dataframe[RATING_COLUMN_TITLE]).count(rating))
 
 graphic = go.Figure([go.Pie(labels=comics_rating, values=rating_count)])
 graphic.update_layout(title_text='Porcetagem das classificatórias dos quadrinhos da Marvel')
