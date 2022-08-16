9b925ffa56ac7579007892ef62ff282a1c686722
MateusVrs <mateusvrs.dev@gmail.com>
2022-08-16 10:47:50 -0300

    code: construct graphic on main

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import sys
+sys.path.append('./')
+
+from dc_mcu.graphic import hair_graphic, eye_graphic
+
 def main():
-    pass
+    hair_graphic.show()
+    eye_graphic.show()
 
 if __name__ == '__main__':
     main()
