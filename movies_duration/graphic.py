e3e541f7e785e9b6efe9a50159248c9ff0d3bd33
otavio-binder <oabinder@gmail.com>
2022-08-16 09:16:47 -0300

    BIANCAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- movies_duration/graphic.py
+++ movies_duration/graphic.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import plotly.graph_objects as go
+from utils import get_column
 
 PATH_MCU_BOX_OFFICE = 'assets\data\mcu_box_office.csv'
 
@@ -19,22 +20,23 @@ list_duration_movie = list()
 cont = 0
 # Lista para guardar os nomes dos filmes em um vetor
 movie_names = list()
-#Verificar nome por nome e ir juntando 
+#Verificar nome por nome e ir juntando
+movie_duration_column = get_column(mcu_dataframe, 'movie_duration')
 for name in mcu_dataframe.movie_title:
       if name[:4] == 'Spid':
-          spider_duration += mcu_dataframe.movie_duration[cont]
+          spider_duration += movie_duration_column[cont]
       elif name[:4] == 'Iron':
-          iron_duration += mcu_dataframe.movie_duration[cont]
+          iron_duration += movie_duration_column[cont]
       elif name[:4] == "Thor":
-          thor_duration += mcu_dataframe.movie_duration[cont]
+          thor_duration += movie_duration_column[cont]
       elif name[:15] == "Captain America":
-          captain_duration += mcu_dataframe.movie_duration[cont]
+          captain_duration += movie_duration_column[cont]
       elif name[:4] == "Aven" or name[:7] == "The Ave":
-          avengers_duration += mcu_dataframe.movie_duration[cont]
+          avengers_duration += movie_duration_column[cont]
       elif name[:4] == "Guar":
-          guardian_duration += mcu_dataframe.movie_duration[cont]
+          guardian_duration += movie_duration_column[cont]
       elif name[:3] == 'Ant':
-          ant_duration += mcu_dataframe.movie_duration[cont]
+          ant_duration += movie_duration_column[cont]
       else:
           movie_names.append(name)
           list_duration_movie.append(mcu_dataframe.movie_duration[cont])
