c303f101a68af0a9cd237f44d54417358d389959
MateusVrs <mateusvrs.dev@gmail.com>
2022-09-12 16:26:18 -0300

    code: restructure and comment

--- dc_mcu/graphic.py
+++ dc_mcu/graphic.py
@@ -2,69 +2,65 @@ import pandas as pd
 import plotly.graph_objects as go
 from utils.functions import get_column_without_nan, unique_value_list
 
-PATH_DC_DATASET = "assets/data/dc-wikia-data.csv"
-PATH_MARVEL_DATASET = "assets/data/marvel-wikia-data.csv"
+PATH_DC_WIKIA_DATASET = 'https://raw.githubusercontent.com/MateusVrs/apc-endgame/MateuSunaMarco/01/assets/data/dc-wikia-data.csv'
+PATH_MCU_WIKIA_DATASET = 'https://raw.githubusercontent.com/MateusVrs/apc-endgame/MateuSunaMarco/01/assets/data/marvel-wikia-data.csv'
 
-dc_wikia_dataframe = pd.read_csv(PATH_DC_DATASET)
-mcu_wikia_dataframe = pd.read_csv(PATH_MARVEL_DATASET)
+dc_wikia_dataframe = pd.read_csv(PATH_DC_WIKIA_DATASET)
+mcu_wikia_dataframe = pd.read_csv(PATH_MCU_WIKIA_DATASET)
 
 def create_comparative_graphic_column_based(first_df, second_df, first_name, second_name, column_title, graphic_title):
-    """Gera um gráfico comparativo entre uma coluna de dois DataFrames
+  """Gera um gráfico comparativo entre uma coluna de dois DataFrames
+
+  Args:
+    first_df: Primeiro DataFrame a ser utilizado
+    second_df: Segundo DataFrame a ser utilizado
+    first_name: Nome do primeiro DataFrame
+    second_name: Nome do segundo DataFrame
+    column_title: Título da coluna em comum entre os DataFrames
+    graphic_title: Título do gráfico
+
+  Return:
+    Gráfico comparativo entre a coluna dos DataFrames
+  """
+  first_column = get_column_without_nan(first_df, column_title)
+  second_column = get_column_without_nan(second_df, column_title)
+
+  first_values = unique_value_list(first_column)
+  second_values = unique_value_list(second_column)
+
+  values_in_common = list()                                                     
+  for value in first_values:                                                    # Percorre uma das listas de valores
+      if value in second_values and value not in values_in_common:              # Verifica se um valor está na outra lista e não está na values_in_common
+          values_in_common.append(value)                                        # Adiciona o valor em comum
+
+  def count_values_appearance(values, column):
+    """Conta quantas vezes valores aparecem em uma coluna de DataFrame
 
     Args:
-        first_df: Primeiro DataFrame a ser utilizado
-        second_df: Segundo DataFrame a ser utilizado
-        first_name: Nome do primeiro DataFrame
-        second_name: Nome do segundo DataFrame
-        column_title: Título da coluna em comum entre os DataFrames
-        graphic_title: Título do gráfico
+      values: Lista de valores únicos que vão ser contados
+      column: Coluna para contagem dos valores
 
     Return:
-        Gráfico comparativo entre a coluna dos DataFrames
+      Lista de contagem dos valores na ordem da original (values)
     """
-    first_column = get_column_without_nan(first_df, column_title)
-    second_column = get_column_without_nan(second_df, column_title)
+    count = list()
+    for value in values:
+      value_count = column.count(value)                                         # Conta quantas vezes um valor aparece na coluna
+      count.append(value_count)                                                 # Adiciona contagem de um valor
+    return count                                                                # Retorna uma lista com a contagem
 
-    first_values = unique_value_list(first_column)
-    second_values = unique_value_list(second_column)
+  first_count = count_values_appearance(values_in_common, first_column)
+  second_count = count_values_appearance(values_in_common, second_column)
+  
+  graphic_figure = go.Figure([
+      go.Bar(name=first_name, x=values_in_common, y=first_count, marker={'color': '#4C9B82'}),
+      go.Bar(name=second_name, x=values_in_common, y=second_count, marker={'color': '#0F8BAE'})
+  ])
+  graphic_figure.update_layout(title_text=graphic_title, template='plotly_dark')
+  return graphic_figure
 
-    values_in_common = list()                                                     
-    for value in first_values:                                                      # Percorre uma das listas de valores
-        if value in second_values and value not in values_in_common:                # Verifica se um valor está na outra lista e não está na values_in_common
-            values_in_common.append(value)                                          # Adiciona o valor em comum
+HAIR_COLUMN_TITLE = 'HAIR'
+EYE_COLUMN_TITLE = 'EYE'
 
-    for value in second_values:
-        if value in first_values and value not in values_in_common:
-            values_in_common.append(value)
-
-    def count_values_appearance(values, column):
-        """Conta quantas vezes valores aparecem em uma coluna de DataFrame
-
-        Args:
-        values: Lista de valores únicos que vão ser contados
-        column: Coluna para contagem dos valores
-
-        Return:
-        Lista de contagem dos valores na ordem da original (values)
-        """
-        count = list()
-        for value in values:
-            value_count = column.count(value)                                         # Conta quantas vezes um valor aparece na coluna
-            count.append(value_count)                                                 # Adiciona contagem de um valor
-        return count
-
-    first_count = count_values_appearance(values_in_common, first_column)
-    second_count = count_values_appearance(values_in_common, second_column)
-    
-    graphic_figure = go.Figure([
-        go.Bar(name=first_name, x=values_in_common, y=first_count, marker={'color': 'aquamarine'}),
-        go.Bar(name=second_name, x=values_in_common, y=second_count, marker={'color': 'cornflowerblue'})
-    ])
-    graphic_figure.update_layout(title_text=graphic_title, template='plotly_dark')
-    return graphic_figure
-
-HAIR_COLUMN = 'HAIR'
-EYE_COLUMN = 'EYE'
-
-hair_graphic = create_comparative_graphic_column_based(dc_wikia_dataframe, mcu_wikia_dataframe, 'DC', 'MCU', HAIR_COLUMN, 'Cor de cabelo: Personagens da Marvel e DC')
-eye_graphic = create_comparative_graphic_column_based(dc_wikia_dataframe, mcu_wikia_dataframe, 'DC', 'MCU', EYE_COLUMN, 'Cor dos olhos: Personagens Marvel e DC')
+hair_graphic = create_comparative_graphic_column_based(dc_wikia_dataframe, mcu_wikia_dataframe, 'DC', 'MCU', HAIR_COLUMN_TITLE, 'Cor de cabelo: Personagens da Marvel e DC')
+eye_graphic = create_comparative_graphic_column_based(dc_wikia_dataframe, mcu_wikia_dataframe, 'DC', 'MCU', EYE_COLUMN_TITLE, 'Cor dos olhos: Personagens Marvel e DC')
