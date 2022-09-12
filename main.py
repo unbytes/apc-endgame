c21f166f77da53898376e072789c5eaf7635f3c8
otavio-binder <oabinder@gmail.com>
2022-09-12 19:44:52 -0300

    Criação do gráfico de fases main py.

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import sys
 sys.path.append('./')
 
 from movies_duration.graphic import mcu_movies_duration_graphic
+from movies_duration.graphic import mcu_phase_duration_graphic
 
 def main():
     mcu_movies_duration_graphic.show()
-
+    mcu_phase_duration_graphic.show()
 if __name__ == '__main__':
     main()
