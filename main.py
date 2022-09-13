c9aff6fe1a65a4900cdb27c19b2e8c494648126e
MateusVrs <mateusvrs.dev@gmail.com>
2022-09-13 17:26:37 -0300

    code: dashboard html structure

--- main.py
+++ main.py
@@ -1,11 +1,47 @@
+from dc_mcu.graphic import hair_graphic, eye_graphic
 import sys
+from dash import Dash, html, dcc
 sys.path.append('./')
 
-from dc_mcu.graphic import hair_graphic, eye_graphic
+app = Dash(__name__)
 
-def main():
-    hair_graphic.show()
-    eye_graphic.show()
+app.layout = html.Main(children=[
+    html.H1(children='Personagens Marvel e DC'),
+    html.Div(className="graphic_container", children=[
+        html.H2(children='Cor do cabelo'),
+        html.Div(className="row-container", children=[
+            html.Div([
+                "Ano inicial: ",
+                dcc.Input(className="year_input",
+                          value="1935", type='number', min=1935, max=2013, step=1)]),
+            html.Div([
+                "Ano final: ",
+                dcc.Input(className="year_input",
+                          value="2013", type='number', min=1935, max=2013, step=1)])
+        ]),
+        dcc.Graph(
+            id='hair_graph',
+            figure=hair_graphic
+        )
+    ]),
+    html.Div(className="graphic_container", children=[
+        html.H2(children='Cor dos olhos'),
+        html.Div(className="row-container", children=[
+            html.Div([
+                "Ano inicial: ",
+                dcc.Input(className="year_input",
+                          value="1935", type='number', min=1935, max=2013, step=1)]),
+            html.Div([
+                "Ano final: ",
+                dcc.Input(className="year_input",
+                          value="2013", type='number', min=1935, max=2013, step=1)])
+        ]),
+        dcc.Graph(
+            id='eye_graph',
+            figure=eye_graphic
+        )
+    ])
+])
 
 if __name__ == '__main__':
-    main()
+    app.run_server(debug=True)
